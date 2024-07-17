4d152df4bc1cce93f004d22e160fd1127fe2e26b
anna <annaz.li@mail.utoronto.ca>
2024-07-16 21:46:50 -0400

    shell script

---
+++ dpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Step 1: Run meson build
+meson build
+
+# Step 2: Change directory to build
+cd build
+
+# Step 3: Run ninja to build the project
+ninja
+
+# Step 4: Change directory to dma_copy_host_benchmark
+cd dma_copy_dpu_benchmark
+
+# Step 5: Execute the built executable with the specified arguments
+
+./doca_dma_copy_dpu_benchmark -f /home/ubuntu/dma/doca/applications/build/dma_copy/src/Test-Files/1024.txt -p 03:00.1 -r e1:00.1
---
+++ host.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Step 1: Run meson build
+meson build
+
+# Step 2: Change directory to build
+cd build
+
+# Step 3: Run ninja to build the project
+ninja
+
+# Step 4: Change directory to dma_copy_host_benchmark
+cd dma_copy_host_benchmark
+
+# Step 5: Execute the built executable with the specified arguments
+./doca_dma_copy_host_benchmark -f /home/annali/testfile.txt -p e1:00.1
+
